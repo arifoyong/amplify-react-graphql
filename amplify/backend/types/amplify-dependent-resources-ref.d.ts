de97f5f7fc4b2632fdcaa46ac9451236bc958311
Arif Wicaksono Oyong <MES10709@murata.com>
2023-08-20 19:06:11 +0800

    Add backend API

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactgraphql": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactgraphqlfe753c98": {
       "AppClientID": "string",
